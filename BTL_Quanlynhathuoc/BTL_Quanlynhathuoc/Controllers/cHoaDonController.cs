e2acd2b99699c4f5cae19d195adbcb4347ffbaa0
le minh thanh <lethanhk66@gmail.com>
2024-10-27 10:29:54 +0700

    Find invoices by user phone number

---
+++ BTL_Quanlynhathuoc/BTL_Quanlynhathuoc/Controllers/cHoaDonController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+using BTL_Quanlynhathuoc.Models;
+
+namespace BTL_Quanlynhathuoc.Controllers
+{
+    public class cHoaDonController : Controller
+    {
+        private QUANLYNHATHUOCEntities db = new QUANLYNHATHUOCEntities();
+
+
+        // GET: DonThuoc/TimKiemThuoc
+        public ActionResult TimKiemThuoc(string sdt)
+        {
+            if (string.IsNullOrEmpty(sdt))
+            {
+                ViewBag.Message = "Vui lòng nhập số điện thoại.";
+                return View();
+            }
+
+            var khachHang = db.tblKhachHangs.FirstOrDefault(kh => kh.sSDT == sdt);
+            if (khachHang == null)
+            {
+                ViewBag.Message = "Không tìm thấy khách hàng với số điện thoại này.";
+                return View();
+            }
+
+            var danhSachThuoc = db.tblDonThuocs
+                .Where(dt => dt.smaKH == khachHang.smaKH)
+                .SelectMany(dt => dt.tblChiTietDTs)
+                .Select(ct => new DonThuocViewModel
+                {
+                    TenThuoc = ct.tblThuoc.stenThuoc,
+                    SoLuong = ct.isoLuong,
+                    GiaBan = ct.fGiaBan
+                })
+                .ToList();
+
+            ViewBag.KhachHang = khachHang;
+            return View(danhSachThuoc);
+        }
+
+
+
+
+
+
+
+    }
+
+}
---
+++ BTL_Quanlynhathuoc/BTL_Quanlynhathuoc/Models/DonThuocViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTL_Quanlynhathuoc.Models
+{
+    public class DonThuocViewModel
+    {
+        public string TenThuoc { get; set; }
+        public double? SoLuong { get; set; }
+        public double? GiaBan { get; set; }
+    }
+}
